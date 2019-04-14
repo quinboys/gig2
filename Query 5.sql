2e6d7db06b93e21b303a2d5b736b7ed70eae1529
NicceSvahn <49589618+NicceSvahn@users.noreply.github.com>
2019-04-14 15:20:41 +0200

    Update Query 5.sql

--- Query 5.sql
+++ Query 5.sql
@@ -1,7 +1,7 @@
 /*5. (View) - Display the growth in sales (as a percentage) for your business, from the 1st month of opening until the end of the year. */
 
 DROP VIEW IF EXISTS SalesTemp;
-CREATE VIEW SalesTemp
+CREATE SQL SECURITY INVOKER VIEW SalesTemp
 	AS
 		SELECT 
 			SaleID,
@@ -12,7 +12,7 @@ CREATE VIEW SalesTemp
 SELECT * FROM SalesTemp;
 
 DROP VIEW IF EXISTS PercentageGrowth;
-CREATE VIEW PercentageGrowth 
+CREATE SQL SECURITY INVOKER VIEW PercentageGrowth 
 	AS
 		SELECT 
 			DATE_FORMAT(Sales.SaleDate, "%Y-%m") AS Month,
