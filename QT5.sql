b75e89cbd0b2c05e86d5c9b3ab9021b761438834
NicceSvahn <49589618+NicceSvahn@users.noreply.github.com>
2019-04-14 22:08:00 +0200

    Create QT5.sql

---
+++ QT5.sql
@@ -0,0 +1,37 @@
+/*5. (View) - Display the growth in sales (as a percentage) for your business, from the 1st month of opening until the end of the year. */
+
+DROP VIEW IF EXISTS SalesTemp;
+CREATE SQL SECURITY INVOKER VIEW SalesTemp
+	AS
+		SELECT 
+			DATE_FORMAT(Sales.SaleDate, "%Y-%m") AS Month,
+			Sales.SaleID,
+			Sales.Total
+		FROM Sales
+        WHERE SaleDate BETWEEN '2018-04-00' AND '2040-00-00' 
+		GROUP BY DATE_FORMAT(Sales.SaleDate, "%Y-%m"); 
+
+SELECT * FROM SalesTemp;
+
+DROP VIEW IF EXISTS PercentageGrowth;
+CREATE SQL SECURITY INVOKER VIEW PercentageGrowth 
+	AS
+		SELECT 
+			DATE_FORMAT(Sales.SaleDate, "%Y-%m") AS Month,
+			Sales.SaleID,
+            Sales.Total,
+			CONCAT(ROUND(((Sales.Total - SalesTemp.Total) / (
+								SELECT SalesTemp.Total 
+								FROM SalesTemp
+								WHERE SaleDate > '2018-04-00' 
+								GROUP BY DATE_FORMAT(SalesTemp.Month, "%Y-%m")
+								LIMIT 1)) * 100, 2), "%") AS Growth
+        FROM Sales, SalesTemp
+		GROUP BY DATE_FORMAT(Sales.SaleDate, "%Y-%m");
+    
+SELECT * FROM PercentageGrowth;
+
+DROP VIEW PercentageGrowth;
+DROP VIEW SalesTemp;
+    
+
