3f551ddbb49cad096341f48a92540bfc789c9505
NicceSvahn <49589618+NicceSvahn@users.noreply.github.com>
2019-04-14 11:52:40 +0200

    Update Query 5.sql

--- Query 5.sql
+++ Query 5.sql
@@ -1,21 +1,13 @@
 /*5. (View) - Display the growth in sales (as a percentage) for your business, from the 1st month of opening until the end of the year. */
 
-CREATE FUNCTION test()
-	retval := CONCAT(CAST(ROUND(IF(@last_entry = 0, "0", ((TotalSalesInMonth - @last_entry) / @last_entry) * 100), 2) AS CHAR), "%")
-    RETURN retval;
-
-/* !FIXME */
-
-CREATE VIEW PercentageSalesGrowth
+DROP VIEW PercentageGrowth;
+CREATE VIEW PercentageGrowth 
 AS
-SELECT Month,
-       CONCAT(CAST(ROUND(IF(@last_entry = 0,
-          "0",
-          ((TotalSalesInMonth - @last_entry) / @last_entry) * 100), 2) AS CHAR), "%") AS "Percentage Growth In Sales",
-       @last_entry := ROUND(TotalSalesInMonth, 2) AS "Total Sales In Month"       
-FROM
-      (SELECT @last_entry := 0) LastEntry,
-      (SELECT DATE_FORMAT(SaleDate, "%Y-%m") AS Month,
-       SUM(Total) AS TotalSalesInMonth
-       FROM SALES
-       GROUP BY DATE_FORMAT(SaleDate, "%Y-%m")) SALES;
+	SELECT 
+		DATE_FORMAT(SaleDate, "%Y-%m") as Month,
+        Total,
+		Total - (SELECT Total FROM Sales prev WHERE prev.Total < Total) AS Percentage
+	FROM Sales;
+      
+      
+SELECT * FROM PercentageGrowth;
